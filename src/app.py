332fe4fe4bad62256d43d50416cb0913c621121e
Leyniker <leyniker11@hotmail.com>
2026-02-27 19:48:58 -0500

    Adding DeployWithRender Files

---
+++ src/app.py
@@ -0,0 +1,33 @@
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+
+ext_style = "https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/darkly/bootstrap.min.css"
+app = dash.Dash(external_stylesheets=[ext_style])
+
+server = app.server # <- For Render
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x = "Fruit", y = "Amount", color = "City", barmode = "group", template = "plotly_dark")
+
+app.layout = html.Div(children=[
+    html.H1(children = 'Hello Dash'),
+    
+    html.Div(children='''Dash: A web application framework for your data.'''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == "__main__":
+    app.run(debug=True) # <- For testing purposes
+    #app.run(debug=True, host='0.0.0.0', port=9000) # <- To Dockerize the Dash
